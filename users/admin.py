8192adc5e1ece9db047f5e217bfe7fc8a120dfb0
Jaz-B <44511160+Jaz-B@users.noreply.github.com>
2021-10-13 21:55:58 -0300

    update user model, email unique, add tests

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.models import User
+from users.forms import CustomUserCreationForm, CustomUserChangeForm
 
-# Register your models here.
+
+class UserAdmin(UserAdmin):
+    """Customize user/admin on django admin."""
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+
+    search_fields = ('email',)
+    list_display = ('email', 'id', 'username', 'is_driver',
+                    'is_verified', 'is_active')
+    list_filter = ('is_active', 'is_staff', 'is_verified', 'is_driver')
+    ordering = ('email', )
+
+    readonly_fields = ('date_joined', 'birth_date')
+
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'username', 'password')
+        }),
+        ('Personal Info', {
+            'fields': ('first_name', 'last_name', 'birth_date', 'date_joined')
+        }),
+        ('Permissions', {
+            'fields': ('is_staff', 'is_driver', 'is_verified')
+        }),
+        ('Status', {
+            'fields': ('is_active', )
+        }),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'is_staff')}
+         ),
+    )
+
+
+admin.site.register(User, UserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,58 @@
+import re
 from django.db import models
+from django.core import validators
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+from .managers import CustomUserManager
 
-# Create your models here.
+# MODELS
+
+
+class User(AbstractUser):
+    # inherits attributes and methods from default django User
+    # custom
+    email = models.EmailField(
+        _('email address'), unique=True,
+        help_text=_("Only UCA accounts allowed"),
+        validators=[
+            validators.RegexValidator(re.compile(
+                r'\w+@uca\.edu\.ar'), _('Enter a valid email.'), 'invalid')
+            # ^[\w.@+-]+$
+        ]
+    )
+
+    is_driver = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
+    # using default=empty instead of null as its not recommended for strings
+    phone_number = models.CharField(max_length=12, blank=True, default='')
+    GENDER_CHOICES = (
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('Others', 'Others')
+    )
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
+    birth_date = models.DateField(null=True)
+
+    USERNAME_FIELD = 'email'
+    # TO DO: REQUIRED_FIELDS = ['username','date_of_birth','first_name','last_name','gender']
+    REQUIRED_FIELDS = ['username']
+
+    # use custom manager to create users
+    objects = CustomUserManager()
+
+    class Meta:
+        # rename DB table
+        db_table = 'user'
+
+    def __str__(self):
+        return self.email
+
+    def get_username(self):
+        return self.email
+
+# TO DO PERMISIONS
+    # def has_perm(self,perm,obj=None):
+    #     return self.is_admin
+
+    # def has_module_perms(self,opp_Label):
+    #     return True

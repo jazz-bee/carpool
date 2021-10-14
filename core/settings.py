701e67a0609131a8f720805bd594c658d5f53515
Jaz-B <44511160+Jaz-B@users.noreply.github.com>
2021-10-13 21:59:17 -0300

    update settings and alter models

--- core/settings.py
+++ core/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Custom user model
+AUTH_USER_MODEL = 'users.User'
 
 # Application definition
 
@@ -84,11 +86,25 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'carpool',
+        'USER': 'jazminbernal',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
+# DATABASES = {
+#         'default': {
+#             'ENGINE': os.environ.get("SQL_ENGINE",'django.db.backends.postgresql_psycopg2'),
+#             'NAME': os.environ.get("DATABASE_NAME"),
+#             'USER': os.environ.get("DATABASE_USER"),
+#             'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
+#             'HOST': os.environ.get("DATABASE_HOST"),
+#             'PORT': os.environ.get("DATABASE_PORT"),
+#         }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators

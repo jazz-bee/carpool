edbfbd04fd549960a36f833d1f6ef3ecbe5dd7d0
Jaz-B <44511160+Jaz-B@users.noreply.github.com>
2022-04-18 21:03:24 -0300

    vehicle migrations and alter name table

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-class Vehicle(models.model):
+class Vehicle(models.Model):
 
     # ID: an id is automatically gereated by django
     # on_delete: behaviour to adopt when the referenced object is deleted
@@ -14,8 +14,13 @@ class Vehicle(models.model):
     color = models.CharField(max_length=20)  # TODO: limitar las "choices
 
     # MaxValueValidator Raises a ValidationError with a code of 'max_value' if value is greater than limit_value
-    seats = models.PositiveSmallIntegerField(default=1), validators = [
+    seats = models.PositiveSmallIntegerField(default=1, validators=[
         MaxValueValidator(10),
         MinValueValidator(1)
-    ]
+    ])
     is_enabled = models.BooleanField(default=True)
+
+    class Meta:
+        # rename DB table
+        # by default django names it [app_name]_[model_name], which would be "core_vehicle"
+        db_table = 'vehicle'

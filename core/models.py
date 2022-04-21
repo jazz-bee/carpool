23030ab18a31883dea82e17887aa2872c0444089
Jaz-B <44511160+Jaz-B@users.noreply.github.com>
2022-04-21 19:12:59 -0300

    create models: HovLane and HovMilestone

--- core/models.py
+++ core/models.py
@@ -6,8 +6,9 @@ class Vehicle(models.Model):
 
     # ID: an id is automatically gereated by django
     # on_delete: behaviour to adopt when the referenced object is deleted
+    # explicitly specify column's name using db_column, else Django appends '_id'
     id_user = models.ForeignKey(
-        "users.User", on_delete=models.CASCADE)
+        "users.User", on_delete=models.CASCADE, db_column='id_user')
     # TODO validate with regex and Raise a ValidationError
     plate_number = models.CharField(max_length=20)
     model = models.CharField(max_length=50)
@@ -24,3 +25,27 @@ class Vehicle(models.Model):
         # rename DB table
         # by default django names it [app_name]_[model_name], which would be "core_vehicle"
         db_table = 'vehicle'
+
+
+class HovLane(models.Model):
+    name = models.CharField(max_length=100)
+    is_enabled = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'HovLane'
+
+
+class HovMilestone(models.Model):
+    name = models.CharField(max_length=100)
+    id_HovLane = models.ForeignKey(
+        "HovLane", on_delete=models.CASCADE, db_column='id_HovLane')
+    location = models.CharField(max_length=200)
+
+    is_enabled = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'HovMilestone'

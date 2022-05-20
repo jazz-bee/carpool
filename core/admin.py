9c2c59904000660864aa118022a4af2f70a15fc3
Jaz-B <44511160+Jaz-B@users.noreply.github.com>
2022-05-20 18:48:31 -0300

    register hovlane and hovemilestion into admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+# decorator to "register a model" so it's available in the admin page
+
+
+@admin.register(models.HovLane)
+class HovLane(admin.ModelAdmin):
+    """manage HovLanes in Django admin"""
+    list_display = ('name', 'is_enabled', 'created_at', 'updated_at')
+
+    # pagination
+    list_per_page = 5
+
+    # name = models.CharField(max_length=100)
+    # is_enabled = models.BooleanField(default=True)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
+
+
+@admin.register(models.HovMilestone)
+class HovMilestone(admin.ModelAdmin):
+    pass

05667e41edde27169334eff4b55baa287e9219df
Jaz-B <44511160+Jaz-B@users.noreply.github.com>
2022-05-20 18:52:17 -0300

    change verbose_name of core app to HOV

--- core/apps.py
+++ core/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+    # verbose_name is the name that is displayed in Django Admin
+    verbose_name = 'HOV'
